783bcb4b1d2156018b06cdf4d4decef1ee971a97
HelenJonathan <efebehelen95@gmail.com>
2021-01-15 02:11:39 +0100

    updates index.js file

--- docker/index.js
+++ docker/index.js
@@ -1,5 +1,5 @@
 const http = require('http');
 const server = http.createServer((request, response) => {
-	response.end('Welcome to SCA Cloud School Application');
+	response.end('Welcome to SCA Cloud School Application, this is my first assessment');
 });
 server.listen(1337);
